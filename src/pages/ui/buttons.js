71f73900420646dcec84749c773c2625368a779c
lnden <258231674@qq.com>
2019-08-23 16:29:26 +0800

    add antd dev and change catalogue

---
+++ src/pages/ui/buttons.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Card, Button} from 'antd'
+
+export default class Buttons extends Component {
+    render() {
+        return (
+            <div>
+                <Card title="基础按钮">
+                    <Button type="primary">Imooc</Button>
+                </Card>
+            </div>
+        )
+    }
+}

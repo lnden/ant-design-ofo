aa37fa4e9c67697e8a717ce0e64a65224674406c
lnden <258231674@qq.com>
2019-09-20 18:19:39 +0800

    add yapi address

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,83 +1,87 @@
-import axios from 'axios'
+import axios from 'axios';
 import { Modal } from 'antd';
 import JsonP from 'jsonp';
-import Utils from './utils'
+import Utils from './utils';
 
 export default class Axios {
     static requestList(_this, url, params, isMock) {
         this.ajax({
             url,
             data: { params },
-            isMock
+            isMock,
         }).then(res => {
             if (res.code === 0) {
-                let list = res.result.list.map((item, index) => {
-                    item.key = index
-                    return item
-                })
+                const list = res.result.list.map((item, index) => {
+                    item.key = index;
+                    return item;
+                });
                 _this.setState({
                     dataSource: list,
-                    pagination: Utils.pagination(res, (current) => {
+                    pagination: Utils.pagination(res, current => {
                         _this.params.page = current;
-                        _this.requestList()
-                    })
-                })
+                        _this.requestList();
+                    }),
+                });
             }
-        })
+        });
     }
 
     static jsonp(options) {
         return new Promise((resolve, reject) => {
-            JsonP(options.url, {
-                param: 'callback',
-            }, function (err, response) {
-                if (response.status === 'success') {
-                    resolve(response)
-                } else {
-                    reject(response.message)
-                }
-            })
-        })
+            JsonP(
+                options.url,
+                {
+                    param: 'callback',
+                },
+                function(err, response) {
+                    if (response.status === 'success') {
+                        resolve(response);
+                    } else {
+                        reject(response.message);
+                    }
+                },
+            );
+        });
     }
 
     static ajax(options) {
-        let loading = document.getElementById('ajaxLoading');
+        const loading = document.getElementById('ajaxLoading');
         if (options.data && options.data.isShowLoading !== false) {
             loading.style.display = 'block';
         }
 
         let baseApi = String;
         if (options.isMock) {
-            baseApi = "https://www.easy-mock.com/mock/5d5ec2393da1210743354970/v1"
+            baseApi = 'https://www.easy-mock.com/mock/5d5ec2393da1210743354970/v1';
+            // baseApi = "http://yapi.demo.qunar.com/mock/96410"
         } else {
-            // baseApi = "https://www.easy-mock.com/mock/5a7278e28d0c633b9c4adbd7/api"
-            baseApi = "http://localhost:3000/mock-api/pages/"
+            baseApi = 'http://localhost:3000/mock-api/pages/';
         }
         return new Promise((resolve, reject) => {
             axios({
-                url: options.isMock?options.url:`${options.url}.json`,
-                method: 'get',
+                url: options.isMock ? options.url : `${options.url}.json`,
+                method: 'post',
                 baseURL: baseApi,
                 timeout: 5000,
-                params: (options.data && options.data.params) || ''
+                params: (options.data && options.data.params) || '',
             }).then(response => {
                 if (options.data && options.data.isShowLoading !== false) {
                     loading.style.display = 'none';
                 }
                 if (response.status === 200) {
-                    let res = response.data;
+                    const res = response.data;
                     if (res.code === 0) {
-                        resolve(res)
+                        resolve(res);
                     } else {
                         Modal.info({
                             title: '提示',
-                            content: res.message
-                        })
+                            content: res.message,
+                        });
                     }
                 } else {
-                    reject(response.data)
+                    reject(response.data);
                 }
-            })
-        })
+            });
+        });
     }
 }
